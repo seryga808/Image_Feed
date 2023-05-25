138a5173dd6423b0ec04d6dfe1075f357050b67f
seryga808 <qwerty377533@gmil.com>
2023-05-25 18:43:11 +0400

    add logo, constraints

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -10,10 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+// qwqwqwq
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization affter application launch
         return true
     }
 

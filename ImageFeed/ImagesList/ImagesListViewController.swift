77e346be4efb8a31aa8f28369f037ef1524ced63
seryga808 <qwerty377533@gmil.com>
2023-05-30 13:24:06 +0400

    sprint_08

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -11,7 +11,6 @@ class ImagesListViewController: UIViewController {
     
     @IBOutlet private var tableView: UITableView!
     
-    
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
 
     override func viewDidLoad() {

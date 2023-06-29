c9e74ae310e7f6c9a498f8d5ee5269bf8b43459d
seryga808 <qwerty377533@gmil.com>
2023-06-29 19:50:45 +0400

    Экран авторизации с логотипом приложения, авторизация с помощью OAuth 2.0.

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -1,12 +1,20 @@
-import UIKit //
+import UIKit
 
 final class ImagesListViewController: UIViewController {
-    private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
-
-    @IBOutlet private var tableView: UITableView!
-
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
+    
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
@@ -15,41 +23,35 @@ final class ImagesListViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
- 
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == ShowSingleImageSegueIdentifier {
-            guard let viewController = segue.destination as? SingleImageViewController,
-                  let indexPath = sender as? IndexPath,
-                  let image = UIImage(named: photosName[indexPath.row]) else { return }
+        if segue.identifier == showSingleImageSegueIdentifier {
+
+            guard let viewController = segue.destination as? SingleImageViewController else { return }
+            guard let indexPath = sender as? IndexPath else { return }
+            
+            let imageName = photosName[indexPath.row]
+            let image = UIImage(named: imageName)
             viewController.image = image
         } else {
             super.prepare(for: segue, sender: sender)
         }
     }
 }
-    
-private var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .long
-        formatter.timeStyle = .none
-        return formatter
-    }()
-
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photosName.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-
+        
         guard let imageListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
-
+        
         configCell(for: imageListCell, with: indexPath)
-
         return imageListCell
     }
 }
@@ -59,10 +61,10 @@ extension ImagesListViewController {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return
         }
-
+        
         cell.cellImage.image = image
         cell.dateLabel.text = dateFormatter.string(from: Date())
-
+        
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         cell.likeButton.setImage(likeImage, for: .normal)
@@ -71,13 +73,14 @@ extension ImagesListViewController {
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return 0
         }
+        
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
---
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+final class OAuth2Service {
+    
+    private enum NetworkError: Error {
+        case codeError
+    }
+    
+    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        var urlComponents = URLComponents(string: unsplashOAuth2TokenURLString)
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "client_id", value: accessKey),
+            URLQueryItem(name: "client_secret", value: secretKey),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let url = urlComponents?.url else {return}
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                error == nil
+            else { 
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            
+            guard (200 ... 299) ~= response.statusCode else { // http error's handling
+                completion(.failure(response.statusCode as! Error))
+                return
+            }
+            
+            do {
+                let responseBody = try JSONDecoder().decode(OAuth2TokenResponseBody.self, from: data)
+                completion(.success(responseBody.accessToken))
+            } catch let error {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
